60d0ad238d43adf4a9a3fe93b3a4b1fbe08847cb
Rachit <rachitdua997@gmail.com>
2025-03-12 11:51:44 +0530

    Update ImageRepository.java

--- src/main/java/com/example/webcamopencv/Repository/ImageRepository.java
+++ src/main/java/com/example/webcamopencv/Repository/ImageRepository.java
@@ -2,7 +2,7 @@ package com.example.webcamopencv.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 import com.example.webcamopencv.Entity.ImageEntity;
 
 @Repository
