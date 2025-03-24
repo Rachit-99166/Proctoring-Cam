3a06c8504ba6bc8b9dd071a33069aa15e7dac38f
Rachit <rachitdua997@gmail.com>
2025-03-25 00:32:44 +0530

    Update ImageEntity.java

--- src/main/java/com/example/webcamopencv/Entity/ImageEntity.java
+++ src/main/java/com/example/webcamopencv/Entity/ImageEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Data; 
 import lombok.NoArgsConstructor; 
 
 @Entity
