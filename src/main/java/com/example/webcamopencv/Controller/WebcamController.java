12b52b2ea0e434f4783434f5f0d6fed678021fd0
Rachit <rachitdua997@gmail.com>
2025-04-05 00:13:03 +0530

    Update WebcamController.java

--- src/main/java/com/example/webcamopencv/Controller/WebcamController.java
+++ src/main/java/com/example/webcamopencv/Controller/WebcamController.java
@@ -15,7 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.PostMapping; 
 import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes; 
 
 import com.example.webcamopencv.Entity.ImageEntity;
 import com.example.webcamopencv.Service.ImageService; 
