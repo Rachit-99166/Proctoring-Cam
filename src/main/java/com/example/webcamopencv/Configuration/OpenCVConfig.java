3bbc0efc3debb896eed2fd6dd701b80a8c498381
Rachit <rachitdua997@gmail.com>
2025-04-06 12:52:43 +0530

    Update OpenCVConfig.java

--- src/main/java/com/example/webcamopencv/Configuration/OpenCVConfig.java
+++ src/main/java/com/example/webcamopencv/Configuration/OpenCVConfig.java
@@ -1,6 +1,6 @@
 package com.example.webcamopencv.Configuration;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration; 
  
 import jakarta.annotation.PostConstruct;
 
